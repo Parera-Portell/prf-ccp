033c0847d5fbc69deffdced8d3fac9090e773eca
J. A. Parera Portell <44067227+Parera-Portell@users.noreply.github.com>
2024-07-11 14:08:34 +0200

    Update ccp.c

--- ccp.c
+++ ccp.c
@@ -12,6 +12,32 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ * 
+ * 
+ * Variables de la funció main:
+ * t0: temps inicial; tf: temps final; w-wsum: variables per controlar
+ * el nombre de paràmetres que es llegeixen del fitxer de paràmetres;
+ * i-j-k-n: variables comptador; nlen: mida de la matriu RF; nerr: variable
+ * per control d'errors; nlay-ncols: nombre de files i columnes del
+ * perfil; delta: freqüència de les dades en segons; dx-dz: increment de
+ * distància lateral i vertical del perfil; depmin-depmax: profunditat
+ * mínima i màxima del perfil; dep: variable de control de la profunditat; 
+ * beg: temps entre inici de les dades i arribada d'ona P; p: paràmetre 
+ * de raig sísmic; z: variable de control de la profunditat del perfil;
+ * inilat-inilon-finlat-finlon:
+ * latitud i longitud d'inici i final del perfil; len: longitud de perfil;
+ * azim: azimut de perfil; stla-stla: coordenades de l'estació; baz:
+ * backazimuth; hw: mostreig lateral del perfil en km; az0-az1: azimuts
+ * per càlculs geogràfics; dist0-dist1: distància per càlculs geogràfics;
+ * x-y-xi-yi: punts projectats respecte el perfil en km; lati-loni:
+ * coordenades per càlculs geogràfics; ds: distància epicentral entre dos
+ * punts al llarg del raig sísmic; dt: temps de viatge entre dos punts al
+ * llarg del raig sísmic; a-b-c-l-q: variables auxiliars per càlculs;
+ * up-us: velocitats P i S transformades a un model de capes planes;
+ * zf: profunditat transformada a un model de capes planes; psph: paràmetre
+ * de raig sísmic en s/rad; acctime: temps de viatge acumulat; amp:
+ * amplitud de l'RF; fzr: radi de la zona de fresnel; wl: longitud d'ona;
+ * iangle: angle d'incidència; xamp: amplitud corregida.
  */
 
 
@@ -57,15 +83,24 @@ void garc(float lat0, float lon0, float lat1, float lon1, float *dist,
 }
 
 /* Funció per projectar un punt sobre un perfil, en funció de la distància
- * del punt a projectar al punt inicial del perfil (dist) en km, de 
- * l'azimut del punt inicial del perfil al punt a projectar (az0) i de 
- * l'azimut del perfil (az1), en graus. Retorna el punt x sobre el perfil 
+ * del punt a projectar al punt inicial del perfil (dist) en km, de
+ * l'azimut del punt inicial del perfil al punt a projectar (az0) i de
+ * l'azimut del perfil (az1), en graus. Retorna el punt x sobre el perfil
  * i la distància y de l'estació respecte del perfil en km */
 void proj(float dist, float az0, float az1, float *x, float *y){
-	
+
+	// float sidea, sideb, anglea, angleb, anglec, sina, sinb;
+	// anglec=90;
+	// angleb=az0-az1;
+	// anglea=180-anglec-fabs(angleb);
+	// dist *= KM2DEG*DEG2RAD; anglea *= DEG2RAD; angleb *= DEG2RAD; anglec *= DEG2RAD;
+	// sina=sin(anglea)*sin(dist)/sin(anglec);
+	// sinb=sin(angleb)*sin(dist)/sin(anglec);
+	// *x = asin(sina)*RAD2DEG*DEG2KM;
+	// *y = asin(sinb)*RAD2DEG*DEG2KM;
+
 	float azdiff;
-	
-	azdiff = az1-az0;
+	azdiff = az0-az1;
 	azdiff *= DEG2RAD;
 	*x = dist*cos(azdiff);
 	*y = dist*sin(azdiff);
@@ -75,11 +110,11 @@ void proj(float dist, float az0, float az1, float *x, float *y){
 /* Funció principal */
 void main(int argc, char **argv){
 	
-	int t0, w, n, wsum, nlen, nerr, nlay, ncols, max=MAX, i, j, k;
+	int t0, w, n, wsum, nlen, nerr, nlay, ncols, max=MAX, i, j, k, ray;
 	float delta, dx, dz, dep, depmin, depmax, beg, p, array[MAX], z,
 	inilat, inilon, finlat, finlon, len, azim, stla, stlo, baz, hw,
 	az0, az1, dist0, dist1, x, y, xi, yi, ds, dt, a, b, c, up, us, zf, nu, 
-	acctime, amp, latj, lonj, fzr, wl, xamp, dzf, stx, l, q, zv, modn,
+	acctime, amp, fzr, wl, xamp, dzf, stx, l, q, zv, modn,
 	x0, x1, y0, y1, fz, gauss;
 	char rf[500], outfile[500], outres[500], model[500], pvar[20];
 	char *prm = argv[1];
@@ -128,13 +163,14 @@ void main(int argc, char **argv){
 	w=fscanf(prm_file, "%f", &zv); wsum += w;
 	w=fscanf(prm_file, "%f", &nu); wsum += w;
 	w=fscanf(prm_file, "%f", &gauss); wsum += w;
+	w=fscanf(prm_file, "%d", &ray); wsum += w;
 	fclose(prm_file);
 	
 	/* Comprovar si tots els paràmetres s'han llegit correctament */
-	if(wsum != 17){
+	if(wsum != 18){
 		printf("Error reading parameter file. Exiting...\n");
 		exit(1);}
-	
+
 	printf("Initial lat/lon: \t%.2f %.2f deg\n", inilat, inilon);
 	printf("Final lat/lon: \t\t%.2f %.2f deg\n", finlat, finlon);
 	printf("Initial time: \t\t%d s\n", t0);
@@ -147,7 +183,8 @@ void main(int argc, char **argv){
 	printf("Ray param. variable: \t%s\n", pvar);
 	printf("Depth scaling exp. term:%.2f\n", zv);
 	printf("Phase weight exp. term: %.2f\n", nu);
-	printf("Gaussian width param.: %.2f\n", gauss);
+	printf("Gaussian width param.: \t%.2f\n", gauss);
+	printf("FZ (0) or rays (1): \t%d\n", ray);
 	
 	/* Comprovar si la llista de RFs existeix */
 	list_file = fopen(rflist, "r");
@@ -163,7 +200,7 @@ void main(int argc, char **argv){
 		exit(1);}
 	
 	/* Lectura i transformació del model de gradient a model de capes */
-	nlay=depmax/dz; /* nombre de capes */
+	nlay=round(depmax/dz); /* nombre de capes */
 	/* Definició d'estructures per desar el model */
 	struct learthmodel{
 			float vp[nlay], vs[nlay];};
@@ -206,12 +243,11 @@ void main(int argc, char **argv){
 				emod.vs[j] = y0 + ((y1-y0)/(x1-x0)) * (z - x0);
 			}
 		}
-		// printf("%.2f\t%.4f\t%.4f\n",j*dz,emod.vp[j],emod.vs[j]);
 	}
 
 	/* ------------------Inicialització del perfil------------------- */
 	garc(inilat,inilon,finlat,finlon,&len,&azim);
-	ncols = len/dx;
+	ncols = round(len/dx);
 	printf("\nProfile length: %.2f km\n", len);
 	printf("Profile azimuth: %.2f deg\n", azim);
 	printf("Profile size: %dx%d\n", ncols, nlay-(int)(depmin/dz));
@@ -242,13 +278,6 @@ void main(int argc, char **argv){
 		if (nerr != 0){
 			printf("\nError reading SAC file: %s\n", rf);
 			exit (nerr);}
-			
-		if(w==0){
-			/* FFT directa*/
-			plan1 = fftw_plan_dft_1d(nlen, fftin, fftin, FFTW_FORWARD, FFTW_ESTIMATE);
-			/* FFT inversa*/
-			plan2 = fftw_plan_dft_1d(nlen, fftin, fftout, FFTW_BACKWARD, FFTW_ESTIMATE);}
-		w++;
 		
 		/* Call getfhv (SAC library) to get variables from header */
 		getfhv(pvar, &p, &nerr, strlen(pvar));
@@ -256,27 +285,36 @@ void main(int argc, char **argv){
 		getfhv("STLA", &stla, &nerr, strlen("STLA"));
 		getfhv("STLO", &stlo, &nerr, strlen("STLO"));
 		
-		/* Inicialització de la matriu que sobre la qual es farà l'FFT */
-		for(j=0;j<nlen;j++){
-			fftin[j] = array[j];
-		}
-		/* Execució de la FFT directa */
-		fftw_execute(plan1);
-		/* Càlcul de la senyal analítica: s'eliminen les freqüències
-		 * negatives i es deixen a 0. S'assumeix que nlen%2=0 */
-		for(j=0;j<nlen;j++){
-			if(j>=nlen/2){
-				fftin[j] = 0;
+		if(ray==0){
+			if(w==0){
+				/* FFT directa*/
+				plan1 = fftw_plan_dft_1d(nlen, fftin, fftin, FFTW_FORWARD, FFTW_ESTIMATE);
+				/* FFT inversa*/
+				plan2 = fftw_plan_dft_1d(nlen, fftin, fftout, FFTW_BACKWARD, FFTW_ESTIMATE);
+			}
+			/* Inicialització de la matriu que sobre la qual es farà l'FFT */
+			for(j=0;j<nlen;j++){
+				fftin[j] = array[j];
+			}
+			/* Execució de la FFT directa */
+			fftw_execute(plan1);
+			/* Càlcul de la senyal analítica: s'eliminen les freqüències
+				* negatives i es deixen a 0. S'assumeix que nlen%2=0 */
+			for(j=0;j<nlen;j++){
+				if(j>=nlen/2){
+					fftin[j] = 0;
+				}
+			}
+			fftw_execute(plan2);
+			/* Renormalització de les dades */
+			/* Càlcul de la fase instantània (senyal analítica / amplitud
+				* instantània) */
+			for(j=0;j<nlen;j++){
+				fftout[j] /= (nlen/2);
+				fftout[j] /= cabs(fftout[j]);
 			}
 		}
-		fftw_execute(plan2);
-		/* Renormalització de les dades */
-		/* Càlcul de la fase instantània (senyal analítica / amplitud
-		 * instantània) */
-		for(j=0;j<nlen;j++){
-			fftout[j] /= (nlen/2);
-			fftout[j] /= cabs(fftout[j]);
-		}
+		w++;
 
 		/* Punt inicial: coordenades de l'estació projectades al perfil */
 		garc(inilat,inilon,stla,stlo,&dist0,&az0);
@@ -313,90 +351,112 @@ void main(int argc, char **argv){
 				/* Càlcul de l'amplitud dins l'interval de temps */
 				acctime += dt;
 				k = round(acctime/delta);
-				phase = (fftout[k]+fftout[k-1]+fftout[k+1])/3;
-				amp = (array[k]+array[k-1]+array[k+1])/3;
+				amp = (array[k]);
 				/* Càlcul de la projecció sobre el perfil.
 				 * El periode (P) de la FZ == 1. wl = T*v */
 				proj(ds,baz,azim,&xi,&yi);
 				x += xi;
 				y += yi;
-				fz = 1/gauss;
-				wl = fz*(1/us);
-				fzr = sqrtf(0.5*wl*(dz*j)+0.0625*wl*wl);
-				/* Factor de saturació amplitud-profunditat */
-				q = powf((dz*j), zv);
-				/* Se suavitzen les amplituds aplicant una funció
-				/* Gaussiana: G(x)=0.5*std*sqrtf(2*pi)*-->
-				/* -->*exp(-x**2/2*std**2) */
-				if(fabs(y)<=hw){
-					for(n=0;n<ncols;n++){
-						l = n*dx-x;
-						a = 0.5*1*fzr;
-						b = -0.5*(l*l)/(a*a);
-						if(b < -20){c=0;}
-						else{c = exp(b)/a*sqrtf(2*3.14159);}
-						/* Correcció de l'amplitud */
-						xamp = amp*q*c;
-						/* Assignació de l'amplitud al perfil i al
-						/* perfil de normalització */
-						if(xamp != 0){
-							perfil[j*ncols+n] += xamp;
-							pnorm[j*ncols+n] += 1;
-							pphase[j*ncols+n] += phase;
+				if(ray==0){
+					phase = (fftout[k]+fftout[k-1]+fftout[k+1])/3;
+					fz = 1/gauss;
+					wl = fz*(1/us);
+					fzr = sqrtf(0.5*wl*(dz*j)+0.0625*wl*wl);
+					/* Factor de saturació amplitud-profunditat */
+					q = powf((dz*j), zv);
+					/* Se suavitzen les amplituds aplicant una funció
+					/* Gaussiana: G(x)=0.5*std*sqrtf(2*pi)*-->
+					/* -->*exp(-x**2/2*std**2) */
+					if(fabs(y)<=hw){
+						for(n=0;n<ncols;n++){
+							l = n*dx-x;
+							a = 0.5*1*fzr;
+							b = -0.5*(l*l)/(a*a);
+							if(b < -20){c=0;}
+							else{c = exp(b)/a*sqrtf(2*PI);}
+							/* Correcció de l'amplitud */
+							xamp = amp*q*c;
+							/* Assignació de l'amplitud al perfil i al
+							/* perfil de normalització */
+							if(xamp != 0){
+								perfil[j*ncols+n] += xamp;
+								pnorm[j*ncols+n] += 1;
+								pphase[j*ncols+n] += phase;
+							}
 						}
+					}
+				}
+				else{
+					n = (int)(x/dx);
+					if(n>=0 && x<=len && fabs(y)<=hw){
+						perfil[j*ncols+n] += amp;
+						pnorm[j*ncols+n] += 1;
 					}
 				}
 			}
 		}
 	}
-	fftw_destroy_plan(plan1);
-	fftw_destroy_plan(plan2);
-	fftw_cleanup();
+	if(ray==0){
+		fftw_destroy_plan(plan1);
+		fftw_destroy_plan(plan2);
+		fftw_cleanup();
+	}
 	printf("%d!\n", w);
 	fclose(list_file);
 	
 	/* Aplicació de la fase a les amplituds */
-	for(i=0; i<nlay; i++){
-		for(w=0; w<ncols; w++){
-			perfil[i*ncols+w] /= sqrtf(pnorm[i*ncols+w]);
-			absphase[i*ncols+w] = powf(fabs(pphase[i*ncols+w])/pnorm[i*ncols+w],nu);
-			/* Suavitzat per files, només una direcció */
-			if(w>0){
-				absphase[i*ncols+w] = 0.2*absphase[i*ncols+w]+(1-0.2)*absphase[i*ncols+w-1];
+	if(ray==0){
+		for(i=0; i<nlay; i++){
+			for(w=0; w<ncols; w++){
+				perfil[i*ncols+w] /= sqrtf(pnorm[i*ncols+w]);
+				absphase[i*ncols+w] = powf(fabs(pphase[i*ncols+w])/pnorm[i*ncols+w],nu);
+				/* Suavitzat per files*/
+				if(w>0){
+					absphase[i*ncols+w] = 0.2*absphase[i*ncols+w]+(1-0.2)*absphase[i*ncols+w-1];
+				}
 			}
-			perfil[i*ncols+w] *= absphase[i*ncols+w];
 		}
-	}
+		for(i=0; i<nlay; i++){
+			for(w=ncols-1; w>=0; w--){
+				/* Suavitzat per files*/
+				if(w<ncols-1){
+					absphase[i*ncols+w] = 0.2*absphase[i*ncols+w]+(1-0.2)*absphase[i*ncols+w+1];
+				}
+				perfil[i*ncols+w] *= absphase[i*ncols+w];
+			}
+		}
 	/* Suavitzat per columnes cap endavant i cap enrere per evitar 
 	 * desplaçament */
-	for(w=0; w<ncols; w++){
-		for(i=0; i<nlay; i++){
-			if(i>0){
-				perfil[i*ncols+w] = 0.5*perfil[i*ncols+w]+(1-0.5)*perfil[(i-1)*ncols+w];
+		for(w=0; w<ncols; w++){
+			for(i=0; i<nlay; i++){
+				if(i>0){
+					perfil[i*ncols+w] = 0.5*perfil[i*ncols+w]+(1-0.5)*perfil[(i-1)*ncols+w];
+				}
 			}
 		}
-	}
-	for(w=0; w<ncols; w++){
-		for(i=nlay-1; i>=0; i--){
-			if(i<nlay-1){
-				perfil[i*ncols+w] = 0.5*perfil[i*ncols+w]+(1-0.5)*perfil[(i+1)*ncols+w];
+		for(w=0; w<ncols; w++){
+			for(i=nlay-1; i>=0; i--){
+				if(i<nlay-1){
+					perfil[i*ncols+w] = 0.5*perfil[i*ncols+w]+(1-0.5)*perfil[(i+1)*ncols+w];
+				}
 			}
 		}
 	}
 	/* Normalització */
-	/*a = 0.0;
-	for(i=0; i<nlay; i++){
-		for(w=0; w<ncols; w++){
-			if(fabs(perfil[w][i])>a){
-				a = perfil[w][i];
-			}
-		}
-	}
-	for(i=0; i<nlay; i++){
-		for(w=0; w<ncols; w++){
-			perfil[w][i] /= a;
-		}
-	}*/
+	// a = 0.0;
+	// for(i=0; i<nlay; i++){
+	// 	for(w=0; w<ncols; w++){
+	// 		if(fabs(perfil[i*ncols+w])>a){
+	// 			a = perfil[i*ncols+w];
+	// 		}
+	// 	}
+	// }
+	// for(i=0; i<nlay; i++){
+	// 	for(w=0; w<ncols; w++){
+	// 		perfil[i*ncols+w] /= a;
+	// 	}
+	// }
+
 	/* Escriptura a un fitxer */
 	out_file = fopen(outfile, "w");
 	out_res = fopen(outres, "w");
@@ -404,10 +464,10 @@ void main(int argc, char **argv){
 	fprintf(out_res, "%s,%s,%s\n", "x","z","a");
 	for(i=0; i<nlay; i++){
 		for(w=0; w<ncols; w++){
-			a = w*dx;
+			a = w*dx+dx/2;
 			b = i*dz+dz/2;
-			stla=inilat+(finlat-inilat)/ncols*w;
-			stlo=inilon+(finlon-inilon)/ncols*w;
+			stla=asin(sin(inilat*DEG2RAD)*cos(a*KM2DEG*DEG2RAD)+cos(inilat*DEG2RAD)*sin(a*KM2DEG*DEG2RAD)*cos(azim*DEG2RAD))*RAD2DEG;
+			stlo=inilon+atan((sin(a*KM2DEG*DEG2RAD)*sin(azim*DEG2RAD))/(cos(inilat*DEG2RAD)*cos(a*KM2DEG*DEG2RAD)-sin(inilat*DEG2RAD)*sin(a*KM2DEG*DEG2RAD)*cos(azim*DEG2RAD)))*RAD2DEG;
 			c = b*KM2DEG;
 			if(i*dz>=depmin){
 				fprintf(out_file, "%f,%f,%f,%f,%f,%f\n", a,b,perfil[i*ncols+w],stla,stlo,c);
